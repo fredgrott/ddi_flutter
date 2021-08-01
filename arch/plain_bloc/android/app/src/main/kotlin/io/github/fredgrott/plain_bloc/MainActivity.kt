a5b802f7b3f579d903df5c459562b17235e61970
Fred Grott <fred.grott@gmail.com>
2021-08-01 13:58:31 -0500

    first pass at liftupstate bloc
    first pass at liftupstate bloc

--- arch/plain_bloc/android/app/src/main/kotlin/io/github/fredgrott/plain_bloc/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.github.fredgrott.plain_bloc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- arch/setstate_bloc/lib/app/domain/entities/appcounter.dart
+++
@@ -1,7 +0,0 @@
-// Copyright 2021 Fredrick Allan Grott. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-abstract class AppCounter {
-  late int appCounter;
-}
